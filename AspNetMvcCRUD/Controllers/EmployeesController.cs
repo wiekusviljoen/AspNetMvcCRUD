bca5982c2ae839c51db225bda502a7d541f07543
wiekusviljoen <wiekusviljoen@gmail.com>
2022-10-07 20:54:40 +0200

    Add project files.

---
+++ AspNetMvcCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,56 @@
+﻿using AspNetMvcCRUD.Data;
+using AspNetMvcCRUD.Models;
+using AspNetMvcCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMvcCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MvcDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MvcDemoDbContext mvcDemoDbContext)
+
+            
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index() 
+        {
+           var employees =  await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ AspNetMvcCRUD/Data/MvcDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetMvcCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMvcCRUD.Data
+{
+    public class MvcDemoDbContext : DbContext
+    {
+        public MvcDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
